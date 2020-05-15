0a6cc49c9bfe70dd8d6eb4ff54f59963429425b4
woodydiallo <65333564+woodydiallo@users.noreply.github.com>
2020-05-14 22:31:30 -0700

    Changed varchar to 30

--- SparkPlug.sql
+++ SparkPlug.sql
@@ -2,6 +2,6 @@ CREATE TABLE SPARK_PLUG (
 	Spark_partNum INT NOT NULL,
 	Drive_size FLOAT NOT NULL,
 	Thread_size INT NOT NULL,
-	Seat_style VARCHAR(10),
+	Seat_style VARCHAR(30),
 	PRIMARY KEY (Spark_partNum)
 );
