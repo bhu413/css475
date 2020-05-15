b4560bebe24aec73b71ce6482b1a85b950bb4034
Brandon Hu <bhu413@gmail.com>
2020-05-14 23:40:15 -0700

    created spark plug data creation sql

---
+++ SparkPlugDataInserts.sql
@@ -0,0 +1,2 @@
+INSERT INTO SPARK_PLUG (Spark_partNum, Seat_style, Thread_size, Drive_size) VALUES ('IZFR6K11', 'Gasket', 14, 0.625);
+INSERT INTO SPARK_PLUG (Spark_partNum, Seat_style, Thread_size, Drive_size) VALUES ('9600', 'Gasket', 19, 16);
