b53cd9fb9755df1719e0b9def974672a6d81407a
woodydiallo <65333564+woodydiallo@users.noreply.github.com>
2020-05-15 00:36:13 -0700

    change

--- Data Populaton/LightDataInserts.sql
+++ Data Populaton/LightDataInserts.sql
@@ -1,1 +1,2 @@
-INSERT INTO LIGHTS (Light_partNum, Socket, Voltage, Wattage, Bulb_type) VALUES ('9003SU.BP2', 9006, 12, 51, 'Halogen');
+INSERT INTO LIGHTS (Light_partNum, Socket, Voltage, Wattage, Bulb_type) VALUES ('9003SU-2', 9003, 12, 60, 'Halogen');
+INSERT INTO LIGHTS (Light_partNum, Socket, Voltage, Wattage, Bulb_type) VALUES ('9006SU.BP2', 9006, 12, 51, 'Halogen');
