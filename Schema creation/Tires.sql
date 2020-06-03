089785dda1fb7f57bd88797e5e8c150f16cbfc15
Brandon Hu <bhu413@gmail.com>
2020-06-03 01:24:45 -0700

    Updated table creation files

--- Schema creation/Tires.sql
+++ Schema creation/Tires.sql
@@ -1,5 +1,5 @@
 CREATE TABLE TIRES (
-	Tire_partNum INT NOT NULL,
+	Tire_partNum VARCHAR(30) NOT NULL,
 	Pressure INT NOT NULL,
 	Construction VARCHAR(30),
 	Diameter INT NOT NULL,
--- Schema creation/Wheels.sql
+++ Schema creation/Wheels.sql
@@ -1,7 +1,7 @@
 CREATE TABLE WHEELS (
 	Rim_size INT NOT NULL, 
-	Wheel_partNum INT NOT NULL, 
-	Tire_partNum INT NOT NULL, 
+	Wheel_partNum VARCHAR(30) NOT NULL, 
+	Tire_partNum VARCHAR(30) NOT NULL, 
 	PRIMARY KEY (Wheel_partNum),
 	FOREIGN KEY (Tire_partNum) REFERENCES TIRES (Tire_partNum)
 );
