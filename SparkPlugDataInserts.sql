3aec1cc04d6ab2e4187fb39228d42860a46724a0
Brandon Hu <bhu413@gmail.com>
2020-06-09 22:37:16 -0700

    Delete SparkPlugDataInserts.sql

--- SparkPlugDataInserts.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO SPARK_PLUG (Spark_partNum, Seat_style, Thread_size, Drive_size) VALUES ('IZFR6K11', 'Gasket', 14, 0.625);
-INSERT INTO SPARK_PLUG (Spark_partNum, Seat_style, Thread_size, Drive_size) VALUES ('9600', 'Gasket', 19, 16);
-INSERT INTO SPARK_PLUG (Spark_partNum, Seat_style, Thread_size, Drive_size) VALUES ('9603', 'Gasket', 19, 16);
